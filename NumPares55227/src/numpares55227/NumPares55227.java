9264913edc16337b282b39f9b20142969f4b0372
Blanquet <Blanquet@Blanquet-PC>
2021-02-24 21:06:35 -0600

    Primeros 47 Numeros Pares

---
+++ NumPares55227/src/numpares55227/NumPares55227.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares55227;
+
+/**
+ *
+ * @author Blanquet
+ */
+public class NumPares55227 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int num = 0 ;
+      while (num < 94) {
+          if (num%2==0) {
+              System.out.println("numero = " + num);
+          }
+              num++; 
+          }
+      }
+}
